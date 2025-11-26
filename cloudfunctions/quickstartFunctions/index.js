dad3aeed6891f6a2580c33f88731a94e35a5b9f6
Mordey Dean <oncwnuHGgNHkgZIvrYqAuP1GfgUE@git.weixin.qq.com>
2025-11-27 00:20:17 +0800

    11

--- cloudfunctions/quickstartFunctions/index.js
+++ cloudfunctions/quickstartFunctions/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const cloud = require("wx-server-sdk");
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV,
@@ -184,3 +185,32 @@ exports.main = async (event, context) => {
       return await deleteRecord(event);
   }
 };
+=======
+// cloudfunctions/getGoods/index.js
+const cloud = require('wx-server-sdk')
+cloud.init({ env: process.env.ENV_ID }) // 使用当前云函数环境
+
+exports.main = async (event, context) => {
+  const db = cloud.database()
+  const { type, page } = event // 可以从event中获取前端传递的参数
+
+  try {
+    // 在云函数中可以进行更复杂的数据库查询
+    let query = db.collection('POST').where({ status: 'selling' })
+    if (type && type !== 'all') {
+      query = query.where({ transactionType: type })
+    }
+    const result = await query.orderBy('createTime', 'desc').get()
+
+    return {
+      success: true,
+      data: result.data
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message
+    }
+  }
+}
+>>>>>>> e187c7824ab687d7a5dc8798205ae87464e03676
